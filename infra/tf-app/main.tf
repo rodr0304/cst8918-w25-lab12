2cf4664a24ab529bd3208d9ebb8ec74fe54e4b8d
Akash705-hub <pate1706@algonquinlive.com>
2026-07-29 15:14:22 -0400

    new branch for testing

---
+++ infra/tf-app/main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "app_rg" {
+  name     = "RODR0304-a12-rg"
+  location = "Canada Central"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.app_rg.location
+  resource_group_name = azurerm_resource_group.app_rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.app_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
