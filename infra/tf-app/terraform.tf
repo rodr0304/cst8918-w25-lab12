a8bf93d4a3044aca3aa799e15c7a7f4c7a60f67a
Akash705-hub <pate1706@algonquinlive.com>
2026-07-29 17:54:29 -0400

    modify property name

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.5.0"
 
-  required_providers {
+  resource_provider_registrations {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
